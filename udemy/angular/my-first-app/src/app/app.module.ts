33713816e2150ebe4ae76ab7dc296a3d7145cba2
wendelfl <wendel.filho@tecnologia.blue>
2023-11-13 15:09:19 -0300

    estudos

--- udemy/angular/my-first-app/src/app/app.module.ts
+++ udemy/angular/my-first-app/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms';
 import { CollapseComponent } from './collapse/collapse.component';
 import { ButtonDirective, CardBodyComponent, CardComponent, CollapseModule } from '@coreui/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ButtonDirective, CardBodyComponent, CardComponent, CollapseModule } fro
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     CollapseModule,
     ButtonDirective,
