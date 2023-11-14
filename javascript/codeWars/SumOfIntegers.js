75a244170ccb8df987c211b14cd693cd4e68767e
Wendel-Fl <wendelfl.dev@gmail.com>
2023-11-14 17:12:42 -0300

    tarefa

--- javascript/codeWars/SumOfIntegers.js
+++ javascript/codeWars/SumOfIntegers.js
@@ -1,5 +1,19 @@
-function getSum(a, b) {
-
+function swap(a, b) {
+    return [b, a];
 }
 
-console.log(getSum(-1, 2));
+function getSum(a, b) {
+    let sum = 0;
+
+    if (b < a) {
+        [a, b] = swap(a, b);
+    }
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+console.log(getSum(2, 2));
