515e9134245c7739e34e51890cee94908f247c4b
WendelFl <wendelfl.dev@gmail.com>
2024-05-30 00:27:46 -0300

    Null Safety

--- alura/mobile/dart/3_exceptions/dart_exceptions/bin/dart_exceptions.dart
+++ alura/mobile/dart/3_exceptions/dart_exceptions/bin/dart_exceptions.dart
@@ -1,8 +1,47 @@
+import 'dart:math';
+
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 import 'exceptions/bank_controller_exception.dart';
 
+void testNullSafety() {
+  Account? myAccount = Account(name: 'Wendel', balance: 200, isAuthenticated: true);
+
+//   Simulando comunicação externa
+  Random rng = Random();
+  int randomNumber = rng.nextInt(10);
+
+  if (randomNumber % 2 == 0) {
+    myAccount.createdAt = DateTime.now();
+  }
+
+  print(myAccount.runtimeType);
+
+  // Teste forçado que não funciona
+  // print(myAccount.balance);
+
+  if (myAccount != null) {
+    print(myAccount.balance);
+    if (myAccount.createdAt != null) {
+      print(myAccount.createdAt!.day);
+    }
+  } else {
+    print('Conta nula');
+  }
+
+  print(myAccount.createdAt);
+  print(myAccount.createdAt!.day);
+
+  // Teste usando operador ternário
+  // print(myAccount != null ? myAccount.balance : 'Conta nula');
+
+  // Teste usando chamada segura
+  // print(myAccount?.balance);
+}
+
 void main() {
+  testNullSafety();
+
   // Criando o banco
   BankController bankController = BankController();
 
@@ -21,7 +60,7 @@ void main() {
   // Fazendo transferência
   try {
     bool result = bankController.makeTransfer(
-        idSender: "Kako", idReceiver: "Ricarth", amount: 1000);
+        idSender: "Kako", idReceiver: "Ricarth", amount: 599);
 
     if (result) {
       print('Transação concluída com sucesso!');
